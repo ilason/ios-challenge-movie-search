c37a7fd1d41b2e8f5f789cf046779fc2da81a176
Aaron Eliason <aaroneliason@hotmail.com>
2016-04-30 12:56:13 -0600

    Set up view

---
+++ MovieSearch/Movie.swift
@@ -0,0 +1,9 @@
+//
+//  Movie.swift
+//  MovieSearch
+//
+//  Created by Aaron Eliason on 4/30/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import Foundation
---
+++ MovieSearch/MovieListViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MovieListViewController.swift
+//  MovieSearch
+//
+//  Created by Aaron Eliason on 4/30/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+class MovieListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+  
+
+}
